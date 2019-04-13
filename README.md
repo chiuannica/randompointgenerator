38e9d68ca428b85b20b28f2202590dd1da9eb451
chiuannica <chiuannica@gmail.com>
2019-04-13 14:34:46 -0400

    added comments to main.js

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # stephcurrysimulator
-A basketball game simulator. Pretty silly. I am a Steph Curry fan. Just tryna learn JS.
+A basketball game simulator
 
 Hello. This is a basketball simulator that has two players. Each player has different stats, percentages for three pointers,
 two pointers, and three pointers.
 
 You can change the stats and regenerate the table.
 
-In basketball, if you miss your shot, the other team gets a chance to score, and it continues. 
-
-Did you know that even Steph Curry, one of the splash brothers and amazing Warriors player, only makes ~45 out of every 100 3-point shot?
-That's many times more than I could, but this man is a professional! Nothing but respect to Mr. Curry.
-
-This is nothing to be taken seriously. Trying to get the hang of JavaScript and git commands. :)
+In basketball, if you miss your shot, the other team gets a chance to score, and it continues.
--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var i, j;
 var p1list = [], p2list = [];
 var rounds = 10;
 
+/*
+Get user input for 3/2/1 pt rate i.e. 45% 3pt like Steph Curry
+*/
 function percents(){
   per1 = document.getElementById('percent1').value;
   document.getElementById("value1").innerHTML = per1 + "%";
@@ -21,20 +24,25 @@ percents();
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
 //Basketball shots are worth 1-3, player may have lost ball
 bballPt = getRandomInt(4);
 
+/*
+function for player 1 that randomly determines if
+they made the shot and how many points they get
+*/
 function p1Round(bballPt){
   var madeit = Math.random();
   p1 = {
-    threePt: per1/100,
+    threePt: per1/100, //user input for player's stats
     twoPt: per2/100,
     onePt: per3/100,
     name: "Freddie Fredderson"
   };
 
   if (bballPt == 3){
-    if (madeit > p1.threePt){ //Missed the basket
+    if (madeit > p1.threePt){       //Missed the basket
       roundPts = 0;
     }else if(madeit <= p1.threePt){ //Made it
       roundPts = 3;
@@ -58,6 +66,11 @@ function p1Round(bballPt){
   return roundPts;
   document.getElementById("p1").innerHTML = "Hello?"
 }
+
+/*
+function for player 2 that randomly determines if
+they made the shot and how many points they get
+*/
 function p2Round(bballPt){
   var madeit = Math.random();
 
@@ -69,7 +82,7 @@ function p2Round(bballPt){
   };
 
   if (bballPt == 3){
-    if (madeit > p2.threePt){ //Missed the basket
+    if (madeit > p2.threePt){       //Missed the basket
       roundPts = 0;
     }else if(madeit <= p2.threePt){ //Made it
       roundPts = 3;
@@ -96,11 +109,11 @@ function p2Round(bballPt){
 function p1(){
   for(i = 0; i < rounds; i++){
     var p1RoundPts;
-    var p1GoesFor = getRandomInt(4);
+    var p1GoesFor = getRandomInt(4); //Randomly decides if p1 goes for a 1, 2, or 3 pt
 
-    p1RoundPts = p1Round(p1GoesFor);
+    p1RoundPts = p1Round(p1GoesFor); //Determines if p1 made it or not
     p1TotalPts += p1RoundPts;
-    p1list.push(p1TotalPts);
+    p1list.push(p1TotalPts);         //Adds round points to list of points
   }
 }
 function p2(){
@@ -118,52 +131,58 @@ function makeTable() {
   var makeTr, makeTd, innerTd;
   var findId;
 
-  makeTr = document.createElement("tr");
+  makeTr = document.createElement("tr");       //1st row of table
 
-  makeTd = document.createElement("td");
-  makeTr.appendChild(makeTd);
-  innerTd = document.createTextNode("Round");
-  makeTd.appendChild(innerTd);
+  makeTd = document.createElement("td");       //column header for the rounds (1, 2, 3, ... 10, ... 9001)
+  makeTr.appendChild(makeTd);                     //Attach this to 1st row
+  innerTd = document.createTextNode("Round");     //Call it round
+  makeTd.appendChild(innerTd);                    //Attach the inside contents ("Round") to column 1
 
-  makeTd = document.createElement("td");
+  makeTd = document.createElement("td");       //column header for p1
   makeTr.appendChild(makeTd);
   innerTd = document.createTextNode(p1.name);
   makeTd.appendChild(innerTd);
 
-  makeTd = document.createElement("td");
+  makeTd = document.createElement("td");       //column header for p2
   makeTr.appendChild(makeTd);
   innerTd = document.createTextNode(p2.name);
   makeTd.appendChild(innerTd);
 
-  findId = document.getElementById("tbl");
+  findId = document.getElementById("tbl");     //find the right table by ID
+  findId.appendChild(makeTr);                  //Attach all of that stuff ^ to the table
 
-  findId.appendChild(makeTr);
-  for(k = 0; k < rounds; k++){
+  for(k = 0; k < rounds; k++){                 //make a row for each round
     var roundNum = k + 1;
 
-    makeTr = document.createElement("tr");
+    makeTr = document.createElement("tr");     //make a row
 
-    makeTd = document.createElement("td");
+    makeTd = document.createElement("td");     //box 1 of row is for the round number
     makeTr.appendChild(makeTd);
     innerTd = document.createTextNode(roundNum);
     makeTd.appendChild(innerTd);
 
-    makeTd = document.createElement("td");
+    makeTd = document.createElement("td");     //box 2 of row is p1's points for that round
     makeTr.appendChild(makeTd);
     innerTd = document.createTextNode(p1list[k]);
     makeTd.appendChild(innerTd);
 
-    makeTd = document.createElement("td");
+    makeTd = document.createElement("td");     //box 3 of the row is p2's points for that round
     makeTr.appendChild(makeTd);
     innerTd = document.createTextNode(p2list[k]);
     makeTd.appendChild(innerTd);
 
-    findId = document.getElementById("tbl");
+    findId = document.getElementById("tbl");   //identify the right table by ID
 
-    findId.appendChild(makeTr);
+    findId.appendChild(makeTr);                //attach the row to the table
   }
 }
+/*
+this function calls all of the functions
 
+hopefully one day, I will be able to call
+this function and generate a new table
+with different values. but i am a potato
+*/
 function reload(){
   alert(p1.threePt);
   percents();
